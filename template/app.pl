b37cc52edbb0fcd8f662ceb08318bee9215c915d
Takayuki Fukumoto <takayuki.fukumoto@kwansei.ac.jp>
2012-09-18 04:19:55 +0900

    Mojolicious::Liteで使うテンプレートなどを追加.

---
+++ template/app.pl
@@ -0,0 +1,30 @@
+use Mojolicious::Lite;
+use utf8;
+
+get '/' => sub {
+	my $self = shift;
+
+	$self->stash(name => '');
+	$self->render();
+};
+
+$app->start;
+
+__DATA__
+
+@@ index.html.ep
+<html>
+<head><title><%= $name %>'s website</title></head>
+<body style='padding: 30px;'>
+<a href='/profile'>Profile</a>
+</body>
+</html>
+
+@@ profile.html.ep
+<html>
+<head><title><%= $name %>のプロフィール</title></head>
+<body style='padding: 30px;'>
+私の名前は<%= $name %>です.<br>
+趣味は<%= $hobby %>で, 好きなプログラミング言語は<%= $language %>です.
+</body>
+</html>
